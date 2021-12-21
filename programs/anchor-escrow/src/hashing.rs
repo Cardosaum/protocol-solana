1c119cb916d45678bc9d153ddf6431baafa09ff5
Matheus Cardoso <cardosaum@pm.me>
2021-12-21 19:08:05 -0300

    found the bug (?)

--- programs/anchor-escrow/src/hashing.rs
+++ programs/anchor-escrow/src/hashing.rs
@@ -57,61 +57,95 @@ impl<F: PrimeField> CircomPoseidonHasher<F> {
         // loggin compute units as we progress
         //
 
-        msg!("> width_u8...");
+        // msg!("> width_u8...");
         let mut width_u8 = [0u8; 1];
         bytes.read_exact(&mut width_u8)?;
         let width = u8::from_be_bytes(width_u8);
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> full_rounds_len...");
+        // msg!("> full_rounds_len...");
         let mut full_rounds_len = [0u8; 1];
         bytes.read_exact(&mut full_rounds_len)?;
         let full_rounds = u8::from_be_bytes(full_rounds_len);
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> partial_rounds_u8...");
+        // msg!("> partial_rounds_u8...");
         let mut partial_rounds_u8 = [0u8; 1];
         bytes.read_exact(&mut partial_rounds_u8)?;
         let partial_rounds = u8::from_be_bytes(partial_rounds_u8);
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> exponentiation_u8...");
+        // msg!("> exponentiation_u8...");
         let mut exponentiation_u8 = [0u8; 1];
         bytes.read_exact(&mut exponentiation_u8)?;
         let exp = i8::from_be_bytes(exponentiation_u8);
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> round_key_len...");
+        // msg!("> round_key_len...");
         let mut round_key_len = [0u8; 4];
         bytes.read_exact(&mut round_key_len)?;
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> round_key_len_usize...");
+        // msg!("> round_key_len_usize...");
         let round_key_len_usize: usize = u32::from_be_bytes(round_key_len) as usize;
         let mut round_keys_buf = vec![0u8; round_key_len_usize];
         bytes.read_exact(&mut round_keys_buf)?;
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> round_keys...");
+        // msg!("> round_keys...");
         let round_keys = to_field_elements::<F>(&round_keys_buf)?;
         let mut mds_matrix_inner_vec_len = [0u8; 4];
         bytes.read_exact(&mut mds_matrix_inner_vec_len)?;
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
-        msg!("> inner_vec_len_usize...");
+        // msg!("> inner_vec_len_usize...");
         let inner_vec_len_usize = u32::from_be_bytes(mds_matrix_inner_vec_len) as usize;
         let mut mds_matrix: Vec<Vec<F>> = vec![];
-        sol_log_compute_units();
+        // sol_log_compute_units();
 
         msg!("> while bytes not empty...");
         while !bytes.is_empty() {
+            msg!(">>> bytes size: {}...", bytes.len());
+            // msg!(">>> Bytes: {:?}...", bytes);
             let mut inner_vec_buf = vec![0u8; inner_vec_len_usize];
             bytes.read_exact(&mut inner_vec_buf)?;
+            // sol_log_compute_units();
 
-            let inner_vec = to_field_elements::<F>(&inner_vec_buf)?;
+            // msg!(">>> max_size_bytes...");
+            let max_size_bytes = F::BigInt::NUM_LIMBS * 8;
+            // sol_log_compute_units();
+
+            // Pad the input with zeros
+            // msg!(">>> padding_len...");
+            let padding_len =
+                (max_size_bytes - (inner_vec_buf.len() % max_size_bytes)) % max_size_bytes;
+            // sol_log_compute_units();
+
+            // msg!(">>> padded_input...");
+            let padded_input: Vec<u8> = inner_vec_buf
+                .iter()
+                .cloned()
+                .chain(core::iter::repeat(0u8).take(padding_len))
+                .collect();
+            // sol_log_compute_units();
+
+            // msg!(">>> res...");
+            let res = padded_input
+                .chunks(max_size_bytes)
+                .map(F::read)
+                .collect::<Result<Vec<_>, _>>()?;
+            // sol_log_compute_units();
+
+            // msg!(">>> inner_vec...");
+            let inner_vec = res;
             mds_matrix.push(inner_vec);
+            // sol_log_compute_units();
+
+            // msg!(">>> check empty bytes: {}...", bytes.is_empty());
+            // sol_log_compute_units();
         }
-        sol_log_compute_units();
+        // sol_log_compute_units();
+        msg!("REACHED HERE!!!");
 
         let params = PoseidonParameters {
             round_keys,
@@ -122,7 +156,6 @@ impl<F: PrimeField> CircomPoseidonHasher<F> {
             sbox: PoseidonSbox(exp),
         };
 
-        msg!("\n\nIHAAA EU APARECO\n\n");
         let output: F = <CRH<F> as CRHTrait>::evaluate(&params, input)?;
         let value = output.into_repr().to_bytes_le();
         Ok(value)
